7e2f32df476d50df9b9308f6a79e10133e56e0c3
Gabe Lopez <git.unfixed408@passmail.net>
2025-03-06 14:57:18 -0600

    initial check-in

---
+++ database/factories/ImageFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Image>
+ */
+class ImageFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'path' => $this->faker->unique()->randomElement(['media/example1.png', 'media/example2.png', 'media/example3.png']),
+        ];
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        Product::factory(4)
+            ->hasVariants(5)
+            ->hasImages(3)
+            ->create();
+
+        // User::factory(10)->create();
+
+        /*User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);*/
+    }
+}
